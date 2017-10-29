ab6f97bb3c4094c8b8fb5789fb0a5a4083659d6f
mrzhouxu <794135759@qq.com>
2017-10-29 08:32:13 +0800

    help

--- app/Http/Controllers/Wx/WechatController.php
+++ app/Http/Controllers/Wx/WechatController.php
@@ -32,9 +32,9 @@ class WechatController extends Controller
                 }
 
                 // {"ToUserName":"gh_9f675e9c1f81","FromUserName":"ofeo0szCbJLREJi4KrMP1BeQbtMo","CreateTime":"1459931042","MsgType":"event","Event":"CLICK","EventKey":"event_msg_1"}
-                if ($message->Event == 'CLICK') {
-                    return $wxServer->click();
-                }
+//                if ($message->Event == 'CLICK') {
+//                    return $wxServer->click();
+//                }
 
             } else if ($message->MsgType == 'text') {
                 return $wxServer->keyword();
