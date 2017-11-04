b06ae5401b4d1672376cf2fa31090c7b3619c329
mrzhouxu <794135759@qq.com>
2017-11-04 09:19:14 +0800

    easywachat

--- app/Http/Controllers/Wx/IndexController.php
+++ app/Http/Controllers/Wx/IndexController.php
@@ -8,8 +8,10 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function index(){
-    	
+    public function index(){}
+
+    public function test(){
+
     }
     
 }
--- resources/assets/js/components/wx/Wxtest.vue
+++ resources/assets/js/components/wx/Wxtest.vue
@@ -29,7 +29,13 @@
 
         },
         mounted(){
-
+            axios.post('/wx/test')
+                .then((res)=>{
+                    console.log(res)
+                })
+                .catch((err)=>{
+                    console.log(err)
+                })
         }
     }
 </script>
